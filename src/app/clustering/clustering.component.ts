3920f28c77da6345c143303c420db3d9935f9843
Chanon Jenakom <chanonjenakom@gmail.com>
2019-01-15 19:29:14 +0700

    Setup canvas

--- src/app/clustering/clustering.component.ts
+++ src/app/clustering/clustering.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import 'p5';
+
 @Component({
   selector: 'app-clustering',
   templateUrl: './clustering.component.html',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClusteringComponent implements OnInit {
 
+  private sketchId = 'sketch-clustering';
+  
+  private sketch: any;
+
   constructor() { }
 
   ngOnInit() {
+    this.sketch = this.createCanvas();
+  }
+
+  createCanvas() {
+    return new p5((sketch: any) => {
+      sketch.setup = () => {
+        sketch.createCanvas(sketch.windowWidth * .9, sketch.windowHeight * .7);
+        sketch.background('#BBBBBB');
+      };
+
+      sketch.draw = () => {
+
+      };
+    }, this.sketchId);
   }
 
 }
